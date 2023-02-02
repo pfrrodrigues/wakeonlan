305e1b4e21c8dc043645dcb85bf3547dc73ac062
Edu Fantini <edufantinicod@gmail.com>
2023-02-02 11:46:43 -0300

    Add HandlerType

---
+++ include/Config.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "../include/Types.hpp"
+
+namespace WakeOnLanImpl{
+
+    class Config {
+    public:
+        HandlerType handler;
+    };
+}
---
+++ include/Types.hpp
@@ -0,0 +1,8 @@
+namespace WakeOnLanImpl {
+
+    enum class HandlerType {
+        Manager = 0,
+        Participant = 1,
+    };
+
+}
