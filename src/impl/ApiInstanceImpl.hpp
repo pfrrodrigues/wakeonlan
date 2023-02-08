17bc03f285567d35185a9a5f8a2e875f75ea2010
Pablo Rodrigues <p32r60r89@gmail.com>
2023-02-07 22:35:12 -0300

    add ApiInstance, Handler and ApiInstanceImpl modules

---
+++ src/impl/ApiInstanceImpl.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <spdlog/spdlog.h>
+#include <../include/Config.hpp>
+#include <../src/handler/Handler.hpp>
+
+class ApiInstanceImpl {
+public:
+    explicit ApiInstanceImpl(const Config &config);
+    ~ApiInstanceImpl();
+    void run();
+private:
+    Config config;
+    std::unique_ptr<Handler> handler;
+    std::shared_ptr<spdlog::logger> _log;
+};
+
