1fa95303dfaa36f5e7756cfa8895f22b05f66cf8
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2023-02-08 16:51:41 -0300

    add example app for interface testing

--- examples/App.cpp
+++ examples/App.cpp
@@ -1,26 +1,34 @@
+#include <tabulate/table.hpp>
+#include <../src/common/Table.hpp>
+#include <../src/service/InterfaceService.hpp>
 #include <iostream>
-#include <memory>
-#include <ApiInstance.hpp>
 
-int main(int argc, char** argv) {
-    /*************************************
-    * Setting up the API configuration
-    *************************************/
-    /*
-     * Example calling syntax
-     * [] - optional
-     * <> - mandatory
-     * ./app ['manager'] <interface>
-     * */
-    Config config(argv, argc);
+int main(int argc, char const *argv[])
+{
+    WakeOnLanImpl::Table &table = WakeOnLanImpl::Table::get();
+    std::shared_ptr<WakeOnLanImpl::NetworkHandler> networkHandler;
 
-    /*************************************
-    * API instantiation
-    *************************************/
-    ApiInstance api(config);
+    struct WakeOnLanImpl::Table::Participant p0 = {.hostname = "p0",
+                                                   .ip = "127.0.0.1",
+                                                   .mac = "FF:FF:FF:FF:FF:FF",
+                                                   .status = WakeOnLanImpl::Table::ParticipantStatus::Awaken },
+                                             p1 = {.hostname = "p1",
+                                                   .ip = "127.0.0.1",
+                                                   .mac = "FF:FF:FF:FF:FF:FF",
+                                                   .status = WakeOnLanImpl::Table::ParticipantStatus::Sleeping },
+                                             p2 = {.hostname = "p2",
+                                                   .ip = "127.0.0.1",
+                                                   .mac = "FF:FF:FF:FF:FF:FF",
+                                                   .status = WakeOnLanImpl::Table::ParticipantStatus::Awaken };
+    if (!table.insert(p0))
+        std::cout << "Failed to insert\n";
+    if (!table.insert(p1))
+        std::cout << "Failed to insert\n";
+    if (!table.insert(p2))
+        std::cout << "Failed to insert\n";
 
-    /*************************************
-    * Run the API
-    *************************************/
-    api.run();
+    WakeOnLanImpl::InterfaceService interface(table, networkHandler);
+    interface.run();
+
+    return 0;
 }
