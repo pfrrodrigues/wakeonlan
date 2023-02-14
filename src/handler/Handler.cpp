69dbc1f31cccb4014b76b94c49cc5640ce3ae426
Pablo Rodrigues <p32r60r89@gmail.com>
2023-02-14 18:22:22 -0300

    fix interface creation

--- src/handler/Handler.cpp
+++ src/handler/Handler.cpp
@@ -11,6 +11,7 @@ ManagerHandler::ManagerHandler(const Config &cfg, WakeOnLanImpl::Table &t)
     networkHandler = std::make_shared<WakeOnLanImpl::NetworkHandler>(4000, config);
     discoveryService = std::make_unique<WakeOnLanImpl::DiscoveryService>(table, networkHandler);
     monitoringService = std::make_unique<WakeOnLanImpl::MonitoringService>(table, networkHandler);
+    interfaceService = std::make_unique<WakeOnLanImpl::ManagerInterfaceService>(table, networkHandler);
 }
 
 ManagerHandler::~ManagerHandler() {
@@ -22,6 +23,7 @@ ParticipantHandler::ParticipantHandler(const Config &cfg, WakeOnLanImpl::Table &
         networkHandler = std::make_shared<WakeOnLanImpl::NetworkHandler>(4000, config);
         discoveryService = std::make_unique<WakeOnLanImpl::DiscoveryService>(table, networkHandler);
         monitoringService = std::make_unique<WakeOnLanImpl::MonitoringService>(table, networkHandler);
+        interfaceService = std::make_unique<WakeOnLanImpl::ParticipantInterfaceService>(table, networkHandler);
     }
 
 ParticipantHandler::~ParticipantHandler() {
