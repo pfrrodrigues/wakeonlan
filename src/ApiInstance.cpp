0b4e5e4e257d4dee9be5a8cc13c75e4c22c809c2
Pablo Rodrigues <p32r60r89@gmail.com>
2023-03-28 11:14:23 -0300

    update Config handling

--- src/ApiInstance.cpp
+++ src/ApiInstance.cpp
@@ -5,8 +5,10 @@
 using namespace WakeOnLanImpl;
 
 namespace WakeOnLan {
-    ApiInstance::ApiInstance(const Config &config)
-            : impl(std::make_unique<ApiInstanceImpl>(config)) {
+    ApiInstance::ApiInstance(const Config &config) {
+        if (!config.isValid())
+            throw std::runtime_error("Config was not properly configured");
+        impl = std::make_unique<ApiInstanceImpl>(config);
         std::mutex mtx;
         std::lock_guard<std::mutex> lk(mtx);
         Table::get();
