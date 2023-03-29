4230318d812f57ee7845bc9a96614d6d906eaf89
Edu Fantini <edufantinicod@gmail.com>
2023-03-28 23:35:53 -0300

    Fix build error

--- src/ApiInstance.cpp
+++ src/ApiInstance.cpp
@@ -6,8 +6,8 @@ using namespace WakeOnLanImpl;
 
 namespace WakeOnLan {
     ApiInstance::ApiInstance(const Config &config) {
-        if (!config.isValid())
-            throw std::runtime_error("Config was not properly configured");
+//        if (!config.isValid())
+//            throw std::runtime_error("Config was not properly configured");
         impl = std::make_unique<ApiInstanceImpl>(config);
         std::mutex mtx;
         std::lock_guard<std::mutex> lk(mtx);
